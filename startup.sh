3f5343934cc6be4c46ef7236edf31d34754a324d
mstephens-xsl <matt.stephens@airelogic.com>
2018-10-18 07:34:27 +0100

    Updated docker compose and startup scripts.

--- startup.sh
+++ startup.sh
@@ -3,6 +3,7 @@ sudo curl -L https://github.com/docker/compose/releases/download/1.23.0-rc2/dock
 sudo chmod +x /usr/local/bin/docker-compose
 cd /home
 sudo mkdir poc
+cd poc
 sudo git clone https://github.com/nhsd-a2si/a2si-uec-appointment-poc
 
 cd a2si-uec-appointment-poc
