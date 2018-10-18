c72b0f37a0abb52482bb0d7434f74b60ce35cb03
mstephens-xsl <matt.stephens@airelogic.com>
2018-10-18 07:28:31 +0100

    Updated docker compose and added startup scripts.

---
+++ startup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+sudo curl -L https://github.com/docker/compose/releases/download/1.23.0-rc2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+cd /home
+sudo mkdir poc
+sudo git clone https://github.com/nhsd-a2si/a2si-uec-appointment-poc
+
+cd a2si-uec-appointment-poc
+docker-compose pull
+docker-compose up -d
+sleep 20m
+
+docker cp ccri-dataload/src/main/resources/Examples/dataload.sql ccrisql:home/dataload.sql
+
+docker exec -it ccrisql psql -U fhirjpa -d careconnect -f home/dataload.sql
